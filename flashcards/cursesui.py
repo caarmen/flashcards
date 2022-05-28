3c68459c6e6d07d9492551714388abe16bd5fe6b
Carmen Alvarez <c@rmen.ca>
2022-05-28 05:16:26 +0200

    Use relative coordinates instead of percentages, for laying out cursers ui

--- flashcards/cursesui.py
+++ flashcards/cursesui.py
@@ -61,12 +61,10 @@ class CursesUi(Ui):
         self._windows.main.refresh()
 
     @classmethod
-    def _get_text_coordinates(
-        cls, offset_y_pct: float, text_length: int
-    ) -> tuple[int, int]:
+    def _get_text_coordinates(cls, offset_y: int, text_length: int) -> tuple[int, int]:
         # pylint doesn't realize that we'll have the LINES and COLS members
         # pylint: disable=no-member
-        begin_y = int(curses.LINES * offset_y_pct)
+        begin_y = int(curses.LINES / 2) + offset_y
         # pylint: disable=no-member
         begin_x = int((curses.COLS - text_length) / 2)
         return begin_y, begin_x
@@ -78,9 +76,9 @@ class CursesUi(Ui):
         )
         return wide_char_count + len(text)
 
-    def _display_text(self, win, offset_y_pct: float, text: str):
+    def _display_text(self, win, offset_y: int, text: str):
         begin_y, begin_x = self._get_text_coordinates(
-            offset_y_pct=offset_y_pct, text_length=self._text_width(text)
+            offset_y=offset_y, text_length=self._text_width(text)
         )
 
         win.move(0, 0)
@@ -96,10 +94,10 @@ class CursesUi(Ui):
             pass
         win.refresh()
 
-    def _display_input_box(self, offset_y_pct: float, length: int):
+    def _display_input_box(self, offset_y: int, length: int):
         self._windows.input.clear()
         begin_y, begin_x = self._get_text_coordinates(
-            offset_y_pct=offset_y_pct, text_length=length
+            offset_y=offset_y, text_length=length
         )
 
         self._windows.input_border.resize(3, length + 3)
@@ -111,41 +109,42 @@ class CursesUi(Ui):
         self._windows.input.mvwin(begin_y, begin_x)
         self._windows.input.move(0, 0)
 
-    def _input_text(self, offset_y_pct: float, length: int) -> str:
-        self._display_input_box(offset_y_pct=offset_y_pct, length=length)
+    def _input_text(self, offset_y: int, length: int) -> str:
+        self._display_input_box(offset_y=offset_y, length=length)
         text_box = UnicodeTextbox(self._windows.input, length=length)
         return text_box.edit(validate=lambda x: x if x != 127 else curses.KEY_BACKSPACE)
 
     def display_flashcard(self, index: int, total: int, flashcard: str):
         self._display_text(
             win=self._windows.progress,
-            offset_y_pct=0.25,
+            offset_y=7,
             text=self.translations("progress").format(index=index, total=total),
         )
         self._display_text(
             win=self._windows.card,
-            offset_y_pct=0.33,
+            offset_y=-3,
             text=self.translations("display_flashcard").format(key=flashcard),
         )
 
     async def input_guess(self, flashcard: str, max_answer_length: int) -> str:
         self._display_text(
             win=self._windows.input_label,
-            offset_y_pct=0.5,
+            offset_y=3,
             text=self.translations("guess_prompt"),
         )
-        return self._input_text(offset_y_pct=0.66, length=max_answer_length + 1)
+        return self._input_text(offset_y=3, length=max_answer_length + 1)
 
     async def input_replay_missed_cards(self) -> bool:
+        self._display_text(
+            win=self._windows.input_label,
+            offset_y=0,
+            text=self.translations("play_again"),
+        )
         self._windows.input.clear()
         self._windows.input_border.clear()
         self._windows.input.refresh()
         self._windows.input_border.refresh()
-        self._display_text(
-            win=self._windows.input_label,
-            offset_y_pct=0.5,
-            text=self.translations("play_again"),
-        )
+        self._windows.input_label.move(0, 0)
         do_replay = (
             self._windows.input.getkey().casefold()
             == self.translations("answer_yes").casefold()
@@ -157,21 +156,21 @@ class CursesUi(Ui):
     def display_right_guess(self, key: str, correct_answer: str):
         self._display_text(
             win=self._windows.guess_result,
-            offset_y_pct=0.1,
+            offset_y=-8,
             text=self.translations("right_guess"),
         )
 
     def display_wrong_guess(self, key: str, guess: str, correct_answer: str):
         self._display_text(
             win=self._windows.guess_result,
-            offset_y_pct=0.1,
+            offset_y=-8,
             text=self.translations("wrong_guess").format(correct_answer=correct_answer),
         )
 
     def display_score(self, correct_count: int, guessed_count: int):
         self._display_text(
             win=self._windows.score,
-            offset_y_pct=0.8,
+            offset_y=7,
             text=self.translations("game_score").format(
                 correct_count=correct_count, guessed_count=guessed_count
             ),
