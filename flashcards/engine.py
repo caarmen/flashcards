178a48de2424c6e3812088ab08dafc77f12e26f9
Carmen Alvarez <c@rmen.ca>
2022-05-28 05:48:33 +0200

    Display flashcard on a background

--- flashcards/engine.py
+++ flashcards/engine.py
@@ -18,9 +18,8 @@ class Engine:
         self.guessed_count = 0
         self.provider = provider
 
-    async def _play_deck(self, deck: dict[str, str]):
+    async def _play_deck(self, deck: dict[str, str], max_answer_length: int):
         keys = list(deck.keys())
-        max_answer_length = max([len(x) for x in deck.values()])
         random.shuffle(keys)
         self.correct_count = 0
         self.guessed_count = 0
@@ -41,14 +40,15 @@ class Engine:
         self.game_ui.display_score(self.correct_count, self.guessed_count)
         if wrong_guesses:
             if await self.game_ui.input_replay_missed_cards():
-                await self._play_deck(wrong_guesses)
+                await self._play_deck(wrong_guesses, max_answer_length)
 
     async def play(self):
         """
         Play a game
         """
         flashcards = self.provider.flashcards()
-        await self._play_deck(flashcards)
+        max_answer_length = max([len(x) for x in flashcards.values()])
+        await self._play_deck(flashcards, max_answer_length)
         self.game_ui.game_over()
 
     async def game_interrupted(self):
